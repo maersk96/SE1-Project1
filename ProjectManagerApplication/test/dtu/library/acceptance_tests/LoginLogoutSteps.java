1cc0bdd2bf9013093645f6fb365904eb83c9bd92
MikkelLaursen16 <47563587+MikkelLaursen16@users.noreply.github.com>
2020-04-02 20:44:31 +0200

    Remane fortsat

--- ProjectManagerApplication/test/dtu/library/acceptance_tests/LoginLogoutSteps.java
+++ ProjectManagerApplication/test/dtu/library/acceptance_tests/LoginLogoutSteps.java
@@ -10,31 +10,31 @@ import io.cucumber.java.en.When;
 
 public class LoginLogoutSteps {
 
-	private ProjectManagerApp libraryApp;
+	private ProjectManagerApp projectManagerApp;
 	private String password;
 
 	/*
 	 * Note that the constructor is apparently never called, but there are no null
-	 * pointer exceptions regarding that libraryApp is not set. When creating the
+	 * pointer exceptions regarding that projectManagerApp is not set. When creating the
 	 * LoginSteps object, the Cucumber libraries are using that constructor with an
-	 * object of class LibraryApp as the default.
+	 * object of class projectManagerApp as the default.
 	 * 
 	 * This also holds for all other step classes that have a similar constructor.
-	 * In this case, the <b>same</b> object of class LibraryApp is used as an
+	 * In this case, the <b>same</b> object of class projectManagerApp is used as an
 	 * argument. This provides an easy way of sharing the same object, in this case
-	 * the object of class LibraryApp, among all step classes.
+	 * the object of class projectManagerApp, among all step classes.
 	 * 
 	 * This principle is called <em>dependency injection</em>. More information can
 	 * be found in "The Cucumber for Java Book" available online from the DTU Library.
 	 * (search using findit.dtu.dk).
 	 */
-	public LoginLogoutSteps(ProjectManagerApp libraryApp) {
-		this.libraryApp = libraryApp;
+	public LoginLogoutSteps(ProjectManagerApp projectManagerApp) {
+		this.projectManagerApp = projectManagerApp;
 	}
 
 	@Given("that the administrator is not logged in")
 	public void thatTheAdministratorIsNotLoggedIn() throws Exception {
-		assertFalse(libraryApp.adminLoggedIn());
+		assertFalse(projectManagerApp.adminLoggedIn());
 	}
 
 	@Given("the password is {string}")
@@ -44,12 +44,12 @@ public class LoginLogoutSteps {
 
 	@Then("the administrator login succeeds")
 	public void theAdministratorLoginSucceeds() throws Exception {
-		assertTrue(libraryApp.adminLogin(password));
+		assertTrue(projectManagerApp.adminLogin(password));
 	}
 
 	@Then("the adminstrator is logged in")
 	public void theAdminstratorIsLoggedIn() throws Exception {
-		assertTrue(libraryApp.adminLoggedIn());
+		assertTrue(projectManagerApp.adminLoggedIn());
 	}
 
 	@Then("the administrator logs in successfully")
@@ -58,21 +58,21 @@ public class LoginLogoutSteps {
 
 	@Then("the administrator login fails")
 	public void theAdministratorLoginFails() throws Exception {
-		assertFalse(libraryApp.adminLogin(password));
+		assertFalse(projectManagerApp.adminLogin(password));
 	}
 
 	@Then("the administrator is not logged in")
 	public void theAdministratorIsNotLoggedIn() throws Exception {
-		assertFalse(libraryApp.adminLoggedIn());
+		assertFalse(projectManagerApp.adminLoggedIn());
 	}
 
 	@Given("that the administrator is logged in")
 	public void thatTheAdministratorIsLoggedIn() throws Exception {
-		assertTrue(libraryApp.adminLogin("adminadmin"));
+		assertTrue(projectManagerApp.adminLogin("adminadmin"));
 	}
 
 	@When("the administrator logs out")
 	public void theAdministratorLogsOut() throws Exception {
-		libraryApp.adminLogout();
+		projectManagerApp.adminLogout();
 	}
 }
