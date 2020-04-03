50fdb793acc391a0302997bcf388b71244e1b1cd
maersk96 <jonas.littau@gmail.com>
2020-04-03 13:36:31 +0200

    added 3 classes

--- Project Manager Files/src/dtu/projectManager/app/ProjectManagerApp.java
+++ Project Manager Files/src/dtu/projectManager/app/ProjectManagerApp.java
@@ -5,9 +5,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ProjectManagerApp {
+	String currentUser = "";
+
+	public void login(String initials) {
+		currentUser = initials;
+	}
+	public void logout() {
+		currentUser = "";
+	}
+	public boolean isAdmin() {
+		return currentUser.equals("ADMIN");
+	}
 
 	public boolean adminLoggedIn() {
 		return false;
 	}
-
 }
